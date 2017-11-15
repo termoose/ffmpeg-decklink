155341079afd1fee52471d43c19ca08c5927a356
Ole Andre Birkedal <o.birkedal@sportradar.com>
2017-11-15 11:17:18 +0100

    Updated DeckLink SDK and brew links to point to my repository

--- Library/Formula/decklink.rb
+++ Library/Formula/decklink.rb
@@ -1,7 +1,7 @@
 class Decklink < Formula
-  homepage "https://github.com/dche/ffmpeg-decklink"
-  url "https://github.com/dche/ffmpeg-decklink/raw/master/decklink-api-10.4.tar.gz"
-  sha256 "0549bb15777a8175e9e4d01c69156b35ff84718196f76586d3c5b88a42977071"
+  homepage "https://github.com/termoose/ffmpeg-decklink"
+  url "https://github.com/termoose/ffmpeg-decklink/raw/master/decklink-api-10.9.5.tar.gz"
+  sha256 "4d275b806e7e4d96818f3c3aa49b6c1e99049b095e768050cbf4212c418cf540"
 
   def install
     include.install Dir["include/*"]
