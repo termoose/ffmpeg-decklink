984aba3ed5f19da787db2891012b27176813cc27
Ole Andre Birkedal <o.birkedal@sportradar.com>
2017-11-15 11:22:21 +0100

    tar.gz -> tar

--- Library/Formula/decklink.rb
+++ Library/Formula/decklink.rb
@@ -1,6 +1,6 @@
 class Decklink < Formula
   homepage "https://github.com/termoose/ffmpeg-decklink"
-  url "https://github.com/termoose/ffmpeg-decklink/raw/master/decklink-api-10.9.5.tar.gz"
+  url "https://github.com/termoose/ffmpeg-decklink/raw/master/decklink-api-10.9.5.tar"
   sha256 "4d275b806e7e4d96818f3c3aa49b6c1e99049b095e768050cbf4212c418cf540"
 
   def install
